4b3c25efae1a8d2c0b3df89f3b0d616c2b9cb0d1
apanacode <sammengi.sailokesh@gmail.com>
2023-11-07 23:18:36 +0530

    code for converting sentence to title case sentence

---
+++ Code.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Code {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentence");
+        String s1 = sc.nextLine();
+        String s2 = "";
+        s1 = s1 + ' ';
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            if (ch = ' ') {
+                s2 = s2 + ch;
+            } else {
+                s2 = Character.toUpperCase(s2);
+                System.out.print(s2 + " ");
+                s2 = "";
+            }
+        }
+
+    }
+}
