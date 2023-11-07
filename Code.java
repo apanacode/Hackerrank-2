b73ac33f383e1ff2ef7b59001556d7a33e4e7316
apanacode <sammengi.sailokesh@gmail.com>
2023-11-07 23:39:19 +0530

    Code for title case sentence modified wth no errors

--- Code.java
+++ Code.java
@@ -4,15 +4,16 @@ class Code {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the sentence");
+        // StringBuffer sb=new StringBuffer();
         String s1 = sc.nextLine();
         String s2 = "";
         s1 = s1 + ' ';
         for (int i = 0; i < s1.length(); i++) {
             char ch = s1.charAt(i);
-            if (ch = ' ') {
+            if (ch != ' ') {
                 s2 = s2 + ch;
             } else {
-                s2 = Character.toUpperCase(s2);
+                s2 = Character.toUpperCase(s2.charAt(0)) + s2.substring(1);
                 System.out.print(s2 + " ");
                 s2 = "";
             }
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Hackerrank-2
 To convert a String to Titile Case
 The aim of the program is to convert a sentence in title case letter..
-for example: sun rises in the east 
-              SUN RISES IN THE EAST
+for example: sun rises in the east
+output should be : Sun Rises In The East
+            
